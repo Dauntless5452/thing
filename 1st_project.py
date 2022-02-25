514f92b641614ca731ae8adcd846cc3a5c4c01b6
Judah <dauntless5452@gmail.com>
2022-02-25 13:35:39 -0800

    done exept itteration

--- 1st_project.py
+++ 1st_project.py
@@ -10,8 +10,9 @@ target_AC = 10
 hit_modifier = 2
 player_AC = 13
 worm_modifier = 2
+worm = 1
+items = ["grenade", "Med kit", "Portable radio tower"]
 
-locations = "on a boulder", "behind a pillar", "in a ditch"
 def rifle_shot():
     global rifle_ammo, remaining_health, current_target, target_AC, hit_modifier
     if rifle_ammo <= 0:
@@ -44,7 +45,7 @@ def worm_attack():
             damage = str(damage)
             print("you take " + damage + " damage, find cover and regenerate your shield!!!")
     else:
-        print("you jump out of the way in the nick off time")
+        print("you jump out of the way in the nick of time")
 
 def Cover():
     print("you dive behid the nearest boulder")
@@ -52,10 +53,8 @@ def Cover():
     shields = shields + 20
     print("you regenerate 20 shields as you recharge your suit")
 
-def find_positioning():
-    global rifle_ammo, player_AC, hit_modifier, locations
-    print("you see a boulder to climb on, a piller to hide behind, or a ditch to retreat to")
-    chosen_location = input(" A. climb on a boulder \n B. hide behind the pillar \n C. retreat to the ditch \n")
+def find_positioning(chosen_location):
+    global rifle_ammo, player_AC, hit_modifier
     if chosen_location == "A":
         print("you get a better place to shoot from")
         hit_modifier = 5
@@ -67,7 +66,32 @@ def find_positioning():
         rifle_ammo = 10
     else:
         print("chose A, B, or C")
+        
 
+def use_item():
+    global remaining_health, health
+    print(items)
+    chosen = input("\n what would you like to use\n")
+    if chosen == 'grenade':
+        items.remove("grenade")
+        g_damage = random.randint(5, 10)
+        remaining_health = remaining_health - g_damage
+        print("you throw a grenade\n")
+        g_damage = str(g_damage)
+        print("\nyou deal " + g_damage + " to the worm")
+        g_damage = int(g_damage)
+    elif chosen == 'Med kit':
+        items.remove('Med kit')
+        health = 10
+        print('your health regenerates to 10')
+    elif chosen == 'Portable radio tower':
+        if worm == 1:
+            print("you can't raido in the middle of a fight!")
+        else:
+            print("you manage to request help")
+    else:
+        print("chose an item")
+        use_item()
 
 
 print("khhhhhhhhhhhhhhhhhhhhhhhh...this is control do you copy")
@@ -105,6 +129,7 @@ while remaining_health >= 0:
         print("B. recharge shield")
         print("C. Check health, stants, and invantory")
         print("D. Find better positioning")
+        print("E. Use Item")
         choice = input("what would you like to do? \n ")
         if choice == "A":
             rifle_shot()
@@ -128,6 +153,7 @@ while remaining_health >= 0:
             health = str(health)
             print("health = " + health)
             health = int(health)
+            print(items)
             hit_modifier = str(hit_modifier)
             print("Curent hit modifier = " + hit_modifier)
             hit_modifier = int(hit_modifier)
@@ -137,16 +163,25 @@ while remaining_health >= 0:
             print ("\n")
         elif choice == "D":
             print("\n")
-            find_positioning()
+            print("you see a boulder to climb on, a piller to hide behind, or a ditch to retreat to")
+            chosen_location = input(" A. climb on a boulder \n B. hide behind the pillar \n C. retreat to the ditch \n")
+            find_positioning(chosen_location)
+            print("\n")
+            time.sleep(3)
+            worm_attack()
+            print("\n")
+        elif choice == "E":
+            print("\n")
+            use_item()
             print("\n")
             time.sleep(3)
             worm_attack()
             print("\n")
         else:
-            print("chose A, B, C, or D")
-
+            print("chose A, B, C, D, or E")
 
 
 if remaining_health <= 0:
+    worm = 2
     print("The worm slumps dead in front of you, and you move on toward your destination.")
 
