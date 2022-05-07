4ca837c31747958280723a9a7f13677911f83302
Judah <dauntless5452@gmail.com>
2022-05-06 21:07:39 -0700

    ex 5 and 6 done

---
+++ learn.py/ex5.py
@@ -0,0 +1,18 @@
+from sys import builtin_module_names
+
+
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_hight = 74
+my_weight = 180
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = "brown"
+
+print("let's talk about %s" % my_name)
+print("He's %d inches tall" % my_hight)
+print("He's %d pounds heavy" % my_weight)
+print("actualy He's not that heavy")
+print("He's got %s eyes and %s hair" % (my_eyes, my_hair))
+print("His teeth are usualy %s depending on the coffee." % my_teeth)
+print("If I add %d, %d, and %d I get %d." % (my_age, my_hight, my_weight, my_age + my_hight + my_weight))
